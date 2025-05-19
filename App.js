257d9ae1453c487c2cbfd676da750be273b0a624
mandy-EG <196489813+mandy-EG@users.noreply.github.com>
2025-05-19 17:50:08 -0500

    Archivos nuevos

---
+++ App.js
@@ -0,0 +1,36 @@
+const opciones = ["piedra", "papel", "tijera"];
+
+const GetHumanChoice = () => {
+    const jugador = prompt("Tus opciones\npiedra\npapel\ntijera\nQue eliges: ");
+    return jugador.toLowerCase();
+}
+
+const ChoiceHuman = GetHumanChoice()
+
+const GetComputerChoice = () => {
+    return Math.floor(Math.random() * opciones.length);
+}
+
+const ChoiceComputer = GetComputerChoice()
+
+if (ChoiceHuman === "piedra" && ChoiceComputer === 2){
+    console.log('Usted gana la computadora saco: tijera');
+}
+else if (ChoiceHuman === 'piedra' && ChoiceComputer === 1){
+    console.log('Usted pierde la computadora saco: papel ');
+}
+else if(ChoiceHuman === "papel" && ChoiceComputer === 0){
+    console.log('Usted gana la computadora saco piedra');
+}
+else if (ChoiceHuman === 'papel' && ChoiceComputer === 2){
+    console.log('Usted pierde la computadora saco: tijera');
+}
+else if(ChoiceHuman === "tijera" && ChoiceComputer === 1){
+    console.log('Usted gana la computadora saco: papel');
+}
+else if (ChoiceHuman === 'tijera' && ChoiceComputer === 0){
+    console.log('Usted pierde la computadora saco piedra');
+}
+else if(ChoiceHuman === ChoiceComputer){
+    console.log('Empate, los 2 sacaron el mismo resultado.');
+}
